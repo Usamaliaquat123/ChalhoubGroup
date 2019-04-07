916b2b57e8e0cc8fd36f6a7dd5644248706faffb
Usama liaquat <Usamaliaquat@usamaliaquat.onmicrosoft.com>
2019-04-07 11:26:57 -0500

    hack ⭐️

--- server/app.js
+++ server/app.js
@@ -1,15 +1,28 @@
 
-const app  = require('express')()
+const express  = require('express')
 const NewsAPI = require('newsapi')
 const dotenv = require('dotenv')
 
+
+
+const app = express()
 dotenv.config();
 const newsapi = new NewsAPI(process.env.NEWS_API_KEY)
 
+const newsCategoryRouter = express.Router()
 
+newsCategoryRouter.route('/categories')
+    .get((req,res) => {
+        const responseJSON = {hello : "This is my API"}
+        res.json(responseJSON)
+    })
 
+app.use('/api',newsCategoryRouter)
 const port = process.env.PORT || 4848;
 
+app.get('/',(req,res) => {
+    res.send('Welcome to API')
+})
 
 
 app.listen(port,() => {
--- server/gulpfile.js
+++ server/gulpfile.js
@@ -1,7 +1,22 @@
 const gulp  = require('gulp');
 const nodemon = require('gulp-nodemon')
-gulp.task('start',() => {
+const config = require('./config.json')
+
+gulp.task('default',() => {
     nodemon({
-        script: 'app.js'
+        script: 'app.js',
+        ext:'js',
+        env:{
+            PORT : 4848
+        },
+        ignore : ['./node_modules/**']
+    })
+    .on('restart',() => {
+        console.log(`restarting ${config.appName}`)
+    })
+    .on('start',() => {
+        console.log(`starting app ${config.appName} `)
+        console.log(`repository ${config.repository} `)
+        console.log(`author ${config.author} `)
     })
 })
