bb7dc285bca58294c0672e37c9e9c6581e645e94
Usama liaquat <Usamaliaquat@usamaliaquat.onmicrosoft.com>
2019-04-07 09:37:59 -0500

    server implementation 🌟

---
+++ server/app.js
@@ -0,0 +1,14 @@
+
+const app  = require('express')()
+const NewsAPI = require('newsapi')
+
+const newsapi = new NewsAPI(process.env.NEWS_API_KEY)
+
+
+const port = process.env.PORT  || 5000
+
+
+
+app.listen(port,() => {
+    console.log(`server is running on port ${port}`)
+})
